b9a0ff9a7b8216a4bfb475783268740b2f75dc3f
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-10-20 00:27:45 +0530

    added video player things

---
+++ app/src/main/java/com/example/buildabit/VideoTesting/VideoTestingActivity.java
@@ -0,0 +1,36 @@
+package com.example.buildabit.VideoTesting;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.buildabit.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class VideoTestingActivity extends AppCompatActivity
+{
+    @BindView(R.id.button)
+    Button play_button;
+
+    @BindView(R.id.videoview)
+    VideoView container;
+
+    MediaController mediaController;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        ButterKnife.bind(this);
+
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(container);
+
+    }
+}
